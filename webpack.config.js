932997e33dabe348f67b3f9a36968630f2ccc782
mathildebuenerd <mathilde.buenerd@gmail.com>
2018-03-27 16:02:14 +0200

    configuration ok pour webpack version 4

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    entry: "./www/js/index",
+    output: {
+        path: path.resolve(__dirname, "./www/dist"),
+        filename: "./bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /ts$/,
+                loader: "awesome-typescript-loader",
+                options: {
+                    configFileName: 'tsconfig.webpack.json'
+                }
+            },
+            {
+                test: /html$/,
+                loader: "html-loader"
+            },
+            {
+                test: /css$/,
+                loader: "css-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js", ".html", ".css"]
+    },
+    devtool: "#inline-source-map"
+}
